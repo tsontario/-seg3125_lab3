2156a149202a68135cad23cf46af32d734ff049f
Timothy Smith <tsontario@gmail.com>
2019-06-30 09:25:17 -0400

    Navigation bar for restaurants list

--- app/screens/RestaurantsList.js
+++ app/screens/RestaurantsList.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Header, Left, Right, Body, Icon, Button, Title } from 'native-base';
 
 export default class RestaurantsListScreen extends React.Component {
+  static navigationOptions = ({navigation}) => {
+    return {
+      header: (
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='arrow-back' onPress={() => navigation.goBack()} />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Select Restaurant</Title>
+          </Body>
+          <Right></Right>
+        </Header>
+      )
+    }
+  }
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
